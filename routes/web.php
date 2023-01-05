7446638f25bc6941b5696a9c380f47a86ad9943b
reqnime28 <120348566+reqnime28@users.noreply.github.com>
2023-01-05 11:20:07 +0700

    Penyelusaian Tampilan Menu

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('layouts.master');
+// })->middleware('auth');
+
+Route::get('/master', function () {
     return view('layouts.master');
-})->middleware('auth');
+});
 
 Auth::routes();
 
