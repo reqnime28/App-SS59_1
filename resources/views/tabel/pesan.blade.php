17fb6049e0368c0a05ae0c56e799873a76d4a55e
reqnime28 <120348566+reqnime28@users.noreply.github.com>
2023-01-15 21:41:44 +0700

    Tambah Button di pesan

--- resources/views/tabel/pesan.blade.php
+++ resources/views/tabel/pesan.blade.php
@@ -263,7 +263,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            <h1>Pesan</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -280,6 +280,7 @@
 
       <!-- Default box -->
       <table class="table">
+        <button type="button" class="btn btn-primary">Tambah Data</button>
         <thead>
           <tr>
             <th scope="col">#</th>
