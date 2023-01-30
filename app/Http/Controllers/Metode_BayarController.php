dca589106285a290f9bd85317ea3317d97c86e80
reqnime28 <120348566+reqnime28@users.noreply.github.com>
2023-01-30 10:18:45 +0700

    Tambah Index Ke Metode Pembayaran Sama Pasang Link Model Ke Controller

--- app/Http/Controllers/Metode_BayarController.php
+++ app/Http/Controllers/Metode_BayarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Metode_Bayar;
 
 class Metode_BayarController extends Controller
 {
@@ -13,7 +14,7 @@ class Metode_BayarController extends Controller
      */
     public function index()
     {
-        return view('tabel.metode_bayar');
+        return view('metode_bayar.index');
     }
 
     /**
